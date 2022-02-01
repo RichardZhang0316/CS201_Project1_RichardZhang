2bd90b43df7f8841a87ac327b110e921e179e457
RichardZhang0316 <zhank20@wfu.edu>
2022-02-01 04:21:35 -0500

    3

---
+++ src/problem5.java
@@ -0,0 +1,24 @@
+public class problem5 {
+    public static void main(String[] args){
+        int[] input={12,9,6,5,3,1};
+        System.out.println(BinarySearch(1,input,0,5));//Your function should take the array and the number to find as inputs. Only these two?
+    }
+
+    public static int BinarySearch(int target,int[] inputArray,int lower,int upper){
+        if(upper>=1) {
+            int mid = (upper+lower) / 2;
+            if(inputArray[mid]==target){
+                return mid;
+            }
+            else if(inputArray[mid]>target){
+                return (BinarySearch(target,inputArray,mid+1,upper));
+            }
+            else{
+                //when inputArray[mid]<target
+                return (BinarySearch(target,inputArray,lower,mid-1));
+            }
+
+        }
+        return -1;
+    }
+}
